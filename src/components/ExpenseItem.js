8d3c0954daaf64b684402f70e6274ade5a7f7583
CheshtaChawla <chawlacheshta55@gmail.com>
2023-03-02 22:39:45 +0530

    cheshta

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
 function ExpenseItem() {
 
-return
-(
+return(
 <div>
     <div>March 28 2021</div>
     <div>
         <h2>Car Insurance</h2>
         <div>$249.67</div>
     </div>
-
 </div>
-); 
+);
 }
 export default ExpenseItem;
+
+
+// function ExpenseItem(){
+//     return(
+//         <div>
+//             <h1>hdbfks</h1>
+//         </div>
+//     )
+// }
+
+// export default ExpenseItem;
