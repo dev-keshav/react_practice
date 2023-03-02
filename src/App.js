416a527ba70d1f5c850af90cb1e79ab6cb7eec34
CheshtaChawla <chawlacheshta55@gmail.com>
2023-03-02 22:21:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+        <p>let's get started</p>
+        <h2>This would not be visible</h2>
+        {/* <ExpenseItem></ExpenseItem> */}
+        <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
